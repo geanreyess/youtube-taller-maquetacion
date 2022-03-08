5e5ff6a626dc5fd4915191815cdf5f0fd5ed21c1
Gean Reyes <sagitario1990_11@hotmail.com>
2022-03-08 17:16:58 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,66 @@
+/* --------------- Menu ------------- */
+((d) => {
+    const $btnMenu = d.querySelector(".menu-btn");
+    const $menu = d.querySelector(".menu");
+
+    $btnMenu.addEventListener("click", (e) => {
+        $btnMenu.firstElementChild.classList.toggle("none");
+        $btnMenu.lastElementChild.classList.toggle("none");
+        $menu.classList.toggle("is-active");
+    });
+
+    d.addEventListener("click", e => {
+        if(!e.target.matches(".menu a")) return false;
+        
+        $btnMenu.firstElementChild.classList.remove("none");
+        $btnMenu.lastElementChild.classList.add("none");
+        $menu.classList.remove("is-active");
+    });
+
+
+})(document);
+
+/* *************ContactForm ************* */
+((d) => {
+    const $form = d.querySelector(".contact-form");
+    const $loader = d.querySelector(".contact-form-loader");
+    const $response = d.querySelector(".contact-form-response");
+
+    $form.addEventListener("submit", e => {
+        e.preventDefault();
+        $loader.classList.remove("none");
+        fetch("https://formsubmit.co/ajax/sagitario1990_11@hotmail.com", {
+            method: "POST",
+            body: new FormData(e.target)
+        })
+        .then(res => (res.ok ? res.json() : Promise.reject(res)))
+        .then(json => {
+            console.log(json);
+            $loader.classList.add("none");
+            location.hash = "#gracias"
+            $form.reset();
+        })
+        .catch(err => {
+            console.log(err);
+            let message = err.statusText|| "Ocurrio un error al enviar, intenta nuevamente";
+            $response.querySelector("h3").innerHTML = `Error ${err.status}: ${message}`;
+        })
+        .finally(() => {
+            $loader.classList.add(".none");
+            setTimeout(() => {
+                location.hash = "#close";
+            }, 2000);
+
+        });
+    });
+
+
+
+
+
+
+
+
+
+
+})(document);
